3e7f9c643ffa0ee8f5c2290b1ddd9ad345feb8aa
hideki1104 <hideki11047@gmail.com>
2020-03-20 22:36:36 +0900

    javascriptオブジェクト編#21

--- js/main.js
+++ js/main.js
@@ -1,11 +1,48 @@
 'use strict'
 
 {
-	const str = 'hello';
+	// const str = 'hello';
 	// console.log(str.length);
 
 	//str.substring(開始位置、終了位置)；
 	// console.log(str.substring(2, 4));
 
-	console.log(str[1]);
+	// console.log(str[1]);
+
+	// const d = [2019, 11, 14];
+
+	// // console.log(d.join('/'));
+
+	// const t = '17:08:24';
+	// // console.log(t.split(':'));
+	// const [hour, minite, second] = t.split(':');
+	// console.log(hour);
+	// console.log(minite);
+	// console.log(second);
+
+	// const scores = [10, 3, 9];
+
+	// let sum = 0;
+
+	// scores.forEach(score => {
+	// 	sum += score;
+	// });
+
+	// const avg = sum / scores.length;
+
+	// console.log(sum);
+	// console.log(avg);
+
+	// console.log(Math.floor(avg));
+	// console.log(Math.ceil(avg));
+	// console.log(Math.round(avg));
+
+	// console.log(Math.random())
+
+	// console.log(Math.floor(Math.random() * 6 ) + 1);
+
+	const d = new Date();
+	// console.log(d);
+
+	console.log(`${d.getMonth() + 1} 月 ${d.getDate()} 日`);
 }
