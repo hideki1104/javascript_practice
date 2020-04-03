854be0b2426af2b6925ea109ba34a1f417463b1e
hideki1104 <hideki11047@gmail.com>
2020-04-03 17:00:56 +0900

    javascriptDOM編終了

--- js/main.js
+++ js/main.js
@@ -142,15 +142,136 @@
 
   //#5#5
 
-  document.querySelector('button').addEventListener('click', () => {
-  	const targetNode = document.getElementById('target')
-  	
-  	// if (targetNode.classList.contains('my-color') === true) {
-  	// 	targetNode.classList.remove('my-color');
-  	// }else{
-  	// 	targetNode.classList.add('mycolor');
-  	// }
-  	targetNode.classList.toggle('my-color');
-  	});
+  // document.querySelector('button').addEventListener('click', () => {
+  // 	const targetNode = document.getElementById('target')
 
+  // 	// if (targetNode.classList.contains('my-color') === true) {
+  // 	// 	targetNode.classList.remove('my-color');
+  // 	// }else{
+  // 	// 	targetNode.classList.add('mycolor');
+  // 	// }
+  // 	// targetNode.classList.toggle('my-color');
+
+  // 	targetNode.textContent = targetNode.dataset.translation;
+  // 	});
+
+ //#11
+ // document.querySelector('button').addEventListener('click',() => {
+ //  	const item2 = document.createElement('li');
+ //  	item2.textContent = 'item2';
+
+ //  	const ulNode = document.querySelector('ul');
+ //  	ulNode.appendChild(item2);
+ //  });
+    // document.querySelector('button').addEventListener('click', () => {
+    // 	const item0 = document.querySelectorAll('li')[0];
+    // 	const copy = item0.cloneNode(true);
+
+    // 	const ul = document.querySelector('ul');
+    // 	const item2 = document.querySelectorAll('li')[2];
+    // 	ul.insertBefore(copy, item2);
+    // });
+
+    //#12
+    // document.querySelector('button').addEventListener('click', () => {
+    // 	const item1 = document.querySelectorAll('li')[1];
+
+    // 	// item1.remove();
+    // 	document.querySelector('ul').removeChild(item1);
+    // });
+
+    //#13
+    // document.querySelector('button').addEventListener('click', () => {
+    // 	const li = document.createElement('li');
+    // 	const text = document.querySelector('input');
+    // 	li.textContent = text.value;
+    // 	document.querySelector('ul').appendChild(li);
+
+    // 	text.value = '';
+    // 	text.focus();
+    // });
+
+    // document.querySelector('button').addEventListener('click', () => {
+    // 	const li = document.createElement('li')
+    // 	const color = document.querySelector('select');
+    // 	li.textContent = `${color.value} - ${color.selectedIndex}`;
+    // 	document.querySelector('ul').appendChild(li);
+    // 	});
+
+      // document.querySelector('button').addEventListener('click', () => {
+      // 	const colors = document.querySelectorAll('input');
+      // 	let selectedColor;
+
+      // 	colors.forEach(color => {
+      // 		if (color.checked === true){
+      // 			selectedColor = color.value;
+      // 		}
+      // 	});
+
+      // 	const li = document.createElement('li');
+      // 	li.textContent = selectedColor;
+      // 	document.querySelector('ul').appendChild(li);
+      // });
+
+      //#16
+      // document.querySelector('button').addEventListener('click', () => {
+      // 	const colors = document.querySelectorAll('input');
+      // 	const selectedColors = [];
+
+      // 	colors.forEach(color => {
+      // 		if (color.checked === true){
+      // 			selectedColors.push(color.value);
+      // 		}
+      // 	});
+
+      // 	const li = document.createElement('li')
+      // 	li.textContent = selectedColors.join(',')
+      // 	document.querySelector('ul').appendChild(li);
+      // });
+
+      //#17
+      // document.querySelector('button').addEventListener('dblclick', () => {
+      // 	console.log('Double Clicked!');
+      // });
+
+      // document.addEventListener('mousemove', () => {
+      // 	console.log('moved!!');
+      // });
+
+      //#18
+
+      // document.addEventListener('keydown', e => {
+      // 	console.log(e.key);
+      // });
+
+      // const text = document.querySelector('textarea');
+
+      // // text.addEventListener('focus', () => {
+      // // 	console.log('focus');
+      // // });
+
+      // // text.addEventListener('blur', () => {
+      // // 	console.log('blur');
+      // // });
+
+      // text.addEventListener('input', () => {
+      // 	console.log('input');
+      // });
+
+      // text.addEventListener('change', () => {
+      // 	console.log('change');
+      // });
+
+      //#20
+      // document.querySelector('form').addEventListener('submit', e => {
+      // 	e.preventDefault();
+      // 	console.log('submit');
+      // });
+
+      //#21
+      document.querySelector('ul').addEventListener('click', e => {
+      	if (e.target.nodeName === 'LI'){
+      		e.target.classList.toggle('done');
+      	}
+      });
 }
